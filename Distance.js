eb03f045ab5a46dfaf2998c614e6e8b3e8d8cea8
AbdulWahab321 <76244094+AbdulWahab321@users.noreply.github.com>
2021-03-28 08:07:50 +0530

    Add files via upload

---
+++ Distance.js
@@ -0,0 +1,65 @@
+class Player {
+    constructor(){
+      this.index = null;
+      
+      
+      this.name = null;
+    }
+  
+
+    getCount(){
+      var playerCountRef = database.ref('playerCount');
+      playerCountRef.on("value",(data)=>{
+        playerCount = data.val();
+      })
+    }
+  
+    updateCount(){
+      database.ref('playerCount').set({
+        playerCount:playerCount
+      });
+    }
+  
+
+    getDistance(){
+        var contestantCountRef = database.ref('player/distance');
+        contestantCountRef.on("value",(data)=>{
+          distance = data.val();
+          
+        })
+      }
+
+   getName(){
+        var contestantCountRef = database.ref('player');
+        var name=input.value();
+        contestantCountRef.on("value",(data)=>{
+         name = data.val();
+        })
+    }
+    
+ 
+
+  updateName(){
+     nameName=input.value(); 
+      var playerIndex=" Player's/player"+" "+input.value();
+      database.ref(playerIndex).set({
+          name:nameName,
+          
+      })
+      
+
+  }
+  updateBugs(){
+    nameName=input.value(); 
+     var playerIndex=" Player's/player"+" "+input.value()+"Bugs"
+     database.ref(playerIndex).set({
+         BugOrSuggestions:input1.value()
+         
+     })
+     
+
+ }
+
+  
+
+  }
---
+++ bugs.js
@@ -0,0 +1,14 @@
+class BUG{
+constructor(){}
+display(){
+input1=createInput("");
+input1.position(200,170);
+input1.size(150,50);
+buttun1=createButton("Submit")
+buttun1.position(360,170)
+buttun1.size(100,60)
+}
+
+
+
+}
